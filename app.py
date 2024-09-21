7569762c4fa5094996d49c82c2da84c6013e02bf
Santos Flores <santosflores@gmail.com>
2024-09-21 16:13:19 -0700

    chainlit is now running with the system prompt.
    Added some changes to improve code readability

--- app.py
+++ app.py
@@ -2,53 +2,71 @@ import os
 from dotenv import load_dotenv
 import chainlit as cl
 import openai
-import asyncio
-import json
-from datetime import datetime
-from prompts import ASSESSMENT_PROMPT, SYSTEM_PROMPT
+from prompts import SYSTEM_PROMPT
 from langsmith import traceable
 
 # Load environment variables
 load_dotenv()
 
-configurations = {
+CONFIGURATIONS = {
     "mistral_7B_instruct": {
         "endpoint_url": os.getenv("MISTRAL_7B_INSTRUCT_ENDPOINT"),
         "api_key": os.getenv("RUNPOD_API_KEY"),
-        "model": "mistralai/Mistral-7B-Instruct-v0.2"
+        "model": "mistralai/Mistral-7B-Instruct-v0.2",
     },
     "mistral_7B": {
         "endpoint_url": os.getenv("MISTRAL_7B_ENDPOINT"),
         "api_key": os.getenv("RUNPOD_API_KEY"),
-        "model": "mistralai/Mistral-7B-v0.1"
+        "model": "mistralai/Mistral-7B-v0.1",
     },
     "openai_gpt-4": {
         "endpoint_url": os.getenv("OPENAI_ENDPOINT"),
         "api_key": os.getenv("OPENAI_API_KEY"),
-        "model": "gpt-4"
-    }
+        "model": "gpt-4",
+    },
 }
 
 # Choose configuration
-config_key = "openai_gpt-4"
-
-# Get selected configuration
-config = configurations[config_key]
+CONFIG_KEY = "openai_gpt-4"
+CONFIG = CONFIGURATIONS[CONFIG_KEY]
 
 # Initialize the OpenAI async client
-client = openai.AsyncClient(api_key=config["api_key"], base_url=config["endpoint_url"])
+client = openai.AsyncClient(api_key=CONFIG["api_key"], base_url=CONFIG["endpoint_url"])
 
-gen_kwargs = {
-    "model": config["model"],
-    "temperature": 0.3,
-    "max_tokens": 500
-}
+GEN_KWARGS = {"model": CONFIG["model"], "temperature": 0.3, "max_tokens": 500}
+
+def initialize_bot():
+    message_history = cl.user_session.get("message_history", [])
+    if not message_history:
+        message_history.insert(0, {"role": "system", "content": SYSTEM_PROMPT})
+        cl.user_session.set("message_history", message_history)
+
+@traceable(run_type="llm")
+async def get_gpt_response_stream(request):
+    message_history = cl.user_session.get("message_history", [])
+    message_history.append(request)
+    response = cl.Message(content="")
+    
+    stream = await client.chat.completions.create(
+        messages=message_history, stream=True, **GEN_KWARGS
+    )
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await response.stream_token(token)
+    
+    message_history.append({"role": "assistant", "content": response.content})
+    cl.user_session.set("message_history", message_history)
+    await response.send()
+
+@traceable
+@cl.on_chat_start
+async def on_chat_start():
+    initialize_bot()
 
 @traceable
 @cl.on_message
 async def on_message(message: cl.Message):
-    # todo
-
+    await get_gpt_response_stream({"role": "user", "content": message.content})
 
 if __name__ == "__main__":
     cl.main()
